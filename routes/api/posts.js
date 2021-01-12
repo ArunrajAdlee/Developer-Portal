0dbe8137353ae4c4667d08fe84135456c985e87e
a_adlee <arunraj.adlee@hotmail.com>
2021-01-12 03:30:33 -0500

    Add routes to posts route:
    1. Add comment to post
    2. Delete comment on post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -201,4 +201,90 @@ router.put('/unlike/:post_id', auth, async (req, res) => {
   }
 });
 
+// @route   POST api/posts/comment/:post_id
+// @desc    Create a comment on a post
+// @access  Private
+
+router.post(
+  '/comment/:post_id',
+  [auth, [check('text', 'Comment text is required').not().isEmpty()]],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const post = await Post.findById(req.params.post_id);
+
+      if (!post) {
+        return res.status(404).json({ msg: 'Post not found' });
+      }
+
+      const user = await User.findById(req.user.id).select('-password');
+      const { text } = req.body;
+
+      const newCommentObj = {
+        text: text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      post.comments.unshift(newCommentObj);
+      await post.save();
+
+      res.json(post);
+    } catch (error) {
+      if (error.kind == 'ObjectId') {
+        return res.status(404).json({ msg: 'Post not found' });
+      }
+      console.log(error);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route   DELETE api/posts/comment/:post_id/:comment_id
+// @desc    Delete a comment on a post
+// @access  Private
+
+router.delete('/comment/:post_id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment not found on this post' });
+    }
+
+    if (comment.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: 'User is not authorized to delete this comment' });
+    }
+
+    const rmvIndex = post.comments
+      .map((comment) => comment.id)
+      .indexOf(req.params.comment_id);
+
+    post.comments.splice(rmvIndex, 1);
+    await post.save();
+
+    res.json(post);
+  } catch (error) {
+    if (error.kind == 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    console.log(error);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
