ee526f9850e4ba223c20637e6e4e1db1983a13a0
a_adlee <arunraj.adlee@hotmail.com>
2021-01-14 23:58:27 -0500

    Fix components filename formatting

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import ScrollToTop from './util/scrollToTop';
-import CustomRoute from './route/CustomRoute';
-import Landing from './layouts/Landing';
+import ScrollToTop from './Util/scrollToTop';
+import CustomRoute from './Route/customRoute';
+import Landing from './Layout/landing';
 import Login from './Login/login';
 
 const App = () => {
