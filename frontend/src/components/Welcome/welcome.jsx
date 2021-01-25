405518d2893b28e542da0d05efa55b76d9afe290
a_adlee <arunraj.adlee@hotmail.com>
2021-01-25 00:03:49 -0500

    Add Welcome page and css

--- frontend/src/components/Welcome/welcome.jsx
+++ frontend/src/components/Welcome/welcome.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 
 const Welcome = () => {
   return (
     <>
-      <h1>MAIN INFO PAGE</h1>
+      <div className="welcome-container">
+        <Grid container alignItems="center" justify="center" className="section small">
+          SECTION 1
+        </Grid>
+        <Grid container alignItems="center" justify="center" className="section dev-or-employer">
+          SECTION 2
+        </Grid>
+        <Grid container alignItems="center" justify="center" className="section med create-account">
+          SECTION 3
+        </Grid>
+        <Grid container alignItems="center" justify="center" className="section our-future">
+          SECTION 4
+        </Grid>
+      </div>
     </>
   );
 };
