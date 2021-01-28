aa3c7162315d59a89def8c2ae6112328fb4c675e
a_adlee <arunraj.adlee@hotmail.com>
2021-01-27 19:19:43 -0500

    Finish welcome page

--- frontend/src/components/Welcome/welcome.jsx
+++ frontend/src/components/Welcome/welcome.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Grid, Typography, Button } from '@material-ui/core';
+import { Grid, Typography, Button, Card, CardContent, CardActions } from '@material-ui/core';
 import collaborate from '../../assets/img/collaborate.png';
 import rocket from '../../assets/img/rocket.png';
+import profile from '../../assets/img/profile.png';
+import forum from '../../assets/img/forum.png';
+import job_search from '../../assets/img/job_search.png';
 
 const Welcome = () => {
   return (
@@ -23,7 +26,7 @@ const Welcome = () => {
             <Button variant="contained">Find out what people are discussing in our forums!</Button>
           </Grid>
         </Grid>
-        <Grid container alignItems="center" justify="center" className="section dev-or-employer">
+        <Grid container alignItems="center" justify="center" className="section launch-postings">
           <Grid item lg={8} xs={12}>
             <Grid item xs={12}>
               <Typography color="inherit" variant="h2">
@@ -32,7 +35,7 @@ const Welcome = () => {
               </Typography>
             </Grid>
             <Grid item xs={12}>
-              <Button className="dev-button" variant="contained">
+              <Button className="spaced-button" variant="contained">
                 Check out how to create postings!
               </Button>
             </Grid>
@@ -42,10 +45,51 @@ const Welcome = () => {
           </Grid>
         </Grid>
         <Grid container alignItems="center" justify="center" className="section med create-account">
-          SECTION 3
+          <Grid item xs={12}>
+            <Typography variant="h4">
+              Ready to join?
+              <Typography variant="subtitle1">Create an account and start participating today</Typography>
+            </Typography>
+            <Button className="spaced-button" variant="contained">
+              Sign-Up
+            </Button>
+          </Grid>
         </Grid>
-        <Grid container alignItems="center" justify="center" className="section our-future">
-          SECTION 4
+        <Grid container alignItems="center" justify="center" className="section large our-future">
+          <Grid item xs={12}>
+            <Typography variant="h3">
+              Dev-Portal is an ongoing project
+              <Typography variant="subtitle1">Here is what we are currently working on!</Typography>
+            </Typography>
+          </Grid>
+          <Grid spacing={5} container alignItems="center" justify="center">
+            <Grid item xs={12} lg={3}>
+              <Card>
+                <CardContent>
+                  <img src={profile} width="250px" height="auto" />
+                  <Typography variant="subtitle2">
+                    A fully-fledged profile page where users can display their skills, work history, git repos, and more
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} lg={3}>
+              <Card>
+                <CardContent>
+                  <img src={forum} width="250px" height="auto" />
+                  <Typography variant="subtitle2">A forum section where users can post and answer questions</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} lg={3}>
+              <Card>
+                <CardContent>
+                  <img src={job_search} width="300px" height="auto" />
+                  <Typography variant="subtitle2">A job postings page where users post and apply for jobs</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
         </Grid>
       </div>
     </>
