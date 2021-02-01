d00a120a8c764cef7e6ff60d5c45b28616ce1603
a_adlee <arunraj.adlee@hotmail.com>
2021-01-31 22:03:30 -0500

    Start working on Profile page

--- frontend/src/components/Profile/profile.jsx
+++ frontend/src/components/Profile/profile.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { Avatar, Grid, CircularProgress, Backdrop } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
+import { profileConstants } from '../../store/constants/index';
+import { getProfileStart } from '../../store/actions/index';
 
 const Profile = () => {
   const dispatch = useDispatch();
-  const profileApiStatus = useSelector((state) => state.profile.profileApiStatus);
+  const user = useSelector((state) => state.auth.user);
+  const profile = useSelector((state) => state.profile);
 
-  return (
-    <>
-      <Grid spacing={0} container alignItems="center" justify="center" className="user-profile-container">
-        Profile
+  useEffect(() => {
+    dispatch(getProfileStart());
+  }, []);
+
+  return profile && profile.profileApiStatus !== profileConstants.GET_PROFILE_START ? (
+    <div className="profile-container">
+      <Grid spacing={0} container alignItems="center" justify="center">
+        <Grid item>
+          <Avatar alt="User Avatar" src={user.avatar} />
+        </Grid>
       </Grid>
-    </>
+    </div>
+  ) : (
+    <Backdrop open={true}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
   );
 };
 
