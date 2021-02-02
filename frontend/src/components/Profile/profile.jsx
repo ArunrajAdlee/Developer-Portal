68173245a179be614b37591dc4492f34cf1947f0
a_adlee <arunraj.adlee@hotmail.com>
2021-02-02 17:15:49 -0500

    Add to profile component

--- frontend/src/components/Profile/profile.jsx
+++ frontend/src/components/Profile/profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Avatar, Grid, CircularProgress, Backdrop } from '@material-ui/core';
+import { Avatar, Grid, CircularProgress, Backdrop, Typography } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { profileConstants } from '../../store/constants/index';
 import { getProfileStart } from '../../store/actions/index';
@@ -18,9 +18,34 @@ const Profile = () => {
     profile.profileApiStatus === profileConstants.GET_PROFILE_FAILURE ? (
     profile.profile ? (
       <div className="profile-container">
-        <Grid spacing={0} container alignItems="center" justify="center">
-          <Grid item>
+        <Grid className="profile-section" container alignItems="center">
+          <Grid item xs={12} sm={4}>
             <Avatar alt="User Avatar" src={profile.profile.user.avatar} />
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <Typography variant="h2">{profile.profile.user.name}</Typography>
+            <Typography variant="subtitle1">{`${profile.profile.status} @ ${profile.profile.company}`}</Typography>
+            <Typography variant="subtitle1">{profile.profile.location}</Typography>
+          </Grid>
+        </Grid>
+        <Grid className="profile-section" container alignItems="center">
+          <Grid item xs={12}>
+            <Typography variant="h5">Experience</Typography>
+          </Grid>
+        </Grid>
+        <Grid className="profile-section" container alignItems="center">
+          <Grid item xs={12}>
+            <Typography variant="h5">Education</Typography>
+          </Grid>
+        </Grid>
+        <Grid className="profile-section" container alignItems="center">
+          <Grid item xs={12}>
+            <Typography variant="h5">Skills</Typography>
+          </Grid>
+        </Grid>
+        <Grid className="profile-section" container alignItems="center">
+          <Grid item xs={12}>
+            <Typography variant="h5">GitHub</Typography>
           </Grid>
         </Grid>
       </div>
