e6001571cbbbfbd8162cf9a0c5ef4971ed473379
a_adlee <arunraj.adlee@hotmail.com>
2021-02-02 17:43:13 -0500

    Add route change on logout

--- frontend/src/components/Layout/Nav.jsx
+++ frontend/src/components/Layout/Nav.jsx
@@ -6,8 +6,9 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
+import { logoutStart } from '../../store/actions';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,6 +25,13 @@ const useStyles = makeStyles((theme) => ({
 const Nav = () => {
   const isAuth = useSelector((state) => state.auth.isAuthenticated);
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const logout = () => {
+    dispatch(logoutStart());
+    history.push('/');
+  };
 
   return (
     <AppBar color="primary" position="static">
@@ -35,7 +43,9 @@ const Nav = () => {
           Dev-Portal
         </Typography>
         {isAuth ? (
-          <Button color="inherit">Logout</Button>
+          <Button onClick={logout} color="inherit">
+            Logout
+          </Button>
         ) : (
           <Link className="link-no-styles" to="/login">
             <Button color="inherit">Login</Button>
